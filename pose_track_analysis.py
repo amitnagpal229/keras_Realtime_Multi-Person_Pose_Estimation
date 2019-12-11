79997c805a02324c13478a99c57cb904bb2978d3
Amit Nagpal <amit.nagpal@gmail.com>
2019-12-11 15:08:08 -0800

    truth

--- pose_track_analysis.py
+++ pose_track_analysis.py
@@ -109,6 +109,72 @@ def shift_point(point, frame):
         return point[0], point[1]
 
 
+truth = {
+    "42_dunk_386_521": {16: [441, 444]},
+    "39_dunk_186_321": {5: [207]},
+    "195_dunk_715_850": {},
+    "167_dunk_672_807": {6: [738]},
+    "164_dunk_237_372": {},
+    "148_dunk_824_959": {},
+    "82_dunk_2740_2875": {8: [2823]},
+    "17_dunk_382_517": {},
+    "130_dunk_479_614": {69: [579]},  # correct
+    "2_three-pointer_97_232": {5: [159]},  # correct
+    "10_dunk_405_540": {73: [459]},  # correct
+    "5_three-pointer_844_979": {8: [885, 888]},  # correct
+    "6_three-pointer_533_668": {4: [570, 573]},  #correct perfect
+    "42_dunk_722_857": {12: [771, 774, 775], 8: [786]},  #correct
+    "44_dunk_1660_1795": {15: [1713]},  # correct
+    "169_dunk_888_1023": {},
+    "71_dunk_1921_2056": {67: [1998]},  # semantically correct
+    "38_dunk_1334_1469": {6: [1386]},  # noisy signal
+    "28_dunk_1842_1977": {12: [1857]},  # early but ok signal
+    "26_dunk_2464_2599": {11: [2559]},  # correct
+    "155_dunk_1579_1714": {1: [1623]},  # early defender but ok signal
+    "120_dunk_1529_1664": {9: [1593]},  # semantically correct
+    "148_dunk_2435_2570": {21: [2508]},  # noisy but correct
+    "147_dunk_1381_1516": {6: [1443]},  # noisy
+    "132_dunk_1414_1549": {31: [1476]},  # arm may not be up
+    "159_dunk_1725_1860": {},
+    "6_three-pointer_1695_1830": {4: [1773]},  # 19 three pointers from here needed
+    "165_three-pointer_1917_2052": {6: [1977]},  # correct perfect
+    "14_three-pointer_2655_2790": {6: [2706]},  # correct perfect
+    "13_three-pointer_4105_4240": {4: [4224]},  # incorrect but ok signal
+    "158_three-pointer_1869_2004": {},
+    "104_three-pointer_2688_2823": {7: [2748, 2751, 2754]},  # correct perfect
+    "104_three-pointer_2794_2929": {35: [2880]},
+    "104_three-pointer_3125_3260": {3: [3180, 3183]},  # good defender jump
+    "13_three-pointer_3829_3964": {},
+    "157_three-pointer_1255_1390": {3: [1329]},  # noisy
+    "165_three-pointer_789_924": {6: [861, 864]},   # correct perfect
+    "9_three-pointer_1339_1474": {43: [1422]},
+    "104_three-pointer_992_1127": {39: [1041]},   # may result in 0 point
+    "8_three-pointer_3014_3149": {15: [3048, 3051]},   # 8 more three pointers from here
+    "165_three-pointer_2596_2731": {9: [2697]},   # a bit tricky defender jump. missing frame
+    "165_three-pointer_2153_2288": {1: [2193], 9: [2193, 2196]},
+    "166_three-pointer_2490_2625": {10: [2565, 2568, 2571]},
+    "8_three-pointer_2456_2591": {9: [2541, 2544, 2547, 2550]},  # perfect defender
+    "107_three-pointer_378_513": {4: [435, 438, 441, 444]},   # perfect stephen curry
+    "164_three-pointer_1622_1757": {6: [1674, 1677, 1680, 1683]},  # correct perfect
+    "159_three-pointer_1880_2015": {4: [1935, 1938, 1941, 1944]},  # correct perfect
+    "13_three-pointer_3189_3324": {34: [3243, 3246, 3249, 3252]},  # correct perfect
+    "158_three-pointer_2804_2939": {5: [2817, 2820, 2823, 2826, 2829]},  # arm was not up
+    "158_three-pointer_2449_2584": {12: [2505]},
+    "153_three-pointer_2186_2321": {24: [2253, 2256, 2259, 2262]},  # correct perfect
+    "5_three-pointer_412_547": {2: [423]},  # noisy.. missing first couple of frames
+    "8_three-pointer_318_453": {13: [405, 408]},
+    "153_three-pointer_827_962": {9: [870, 873, 876]},
+    "5_three-pointer_1212_1347": {6: [1254, 1257, 1260, 1263, 1266]},  # correct perfect
+    "76_dunk_1473_1608": {14: [1539, 1542, 1545]},
+    "74_dunk_2657_2792": {36: [2727, 2730, 2733]},  # correct perfect
+    "167_dunk_4076_4211": {16: [4122, 4125, 4128, 4131, 4143]},  # noisy, but ok
+    "183_dunk_1596_1731": {26: [2202]},
+    "178_dunk_1944_2079": {},
+    "142_dunk_1719_1854": {15: [1782, 1785]},
+    "7_dunk_143_278": {8: [192, 195, 198]}
+}
+
+
 def process(tracks, unmatched_poses):
     player_travels = {}
     player_positions = {}
@@ -125,7 +191,12 @@ def process(tracks, unmatched_poses):
     key_frames = []
     frame_ids = sorted(all_baskets.keys())
     frame_offset = frame_ids[0]
+    no_truth = video_short_name not in truth
     for track in tracks:
+        truth_frame = list()
+        if video_short_name in truth and track in truth[video_short_name]:
+            truth_frame = truth[video_short_name][track]
+
         player_position = np.zeros((3, len(frame_ids)), dtype=np.int16)
         shifted_position = np.zeros((3, len(frame_ids)), dtype=np.int16)
         player_positions[track] = player_position
@@ -150,6 +221,7 @@ def process(tracks, unmatched_poses):
 
         previous_jump_frame = -1
         for i in range(3, len(frame_ids)):
+            frame_i = frame_ids[i]
             max_travel = np.max(player_travel[1][i - 3:i])  # must be less than -25
             min_travel = np.min(player_travel[1][i - 3:i])  # must be less than -50
             arms_count = player_travel[2][i]  # must be greater than or equal to 1
@@ -159,9 +231,10 @@ def process(tracks, unmatched_poses):
             if not left_basket:
                 x = im_width - x
 
-            if np.argwhere(player_travel[1][i-3:i] < -25).shape[0] == 3 and min_travel < -50 and arms_count >= 1 \
-                    and shifted_position[1][i] > 0:
-                frame_i = frame_ids[i]
+            has_jump = np.argwhere(player_travel[1][i - 3:i] < -25).shape[0] == 3 and min_travel < -50 \
+                       and arms_count >= 1 and shifted_position[1][i] > 0
+
+            if frame_i in truth_frame or (has_jump and no_truth):
                 #print(f"Track: {track}, jumped around frame: {frame_i}")
                 jump = {'end_frame_id': frame_ids[i], 'track_id': track, 'basket': get_basket(frame_i),
                         'player_position': player_position[:, i], 'shifted_position': position,
@@ -304,6 +377,7 @@ if __name__ == '__main__':
     out_keyframes_file = track_file.rsplit(".", 1)[0] + "_keyframes.txt"
     out_train_prefix = track_file.rsplit("_", 2)[-3]
     video_name = out_train_prefix.rsplit("/", 1)[-1]
+    video_short_name = video_name.rsplit(".", 1)[-1]
 
     print(f"Processing {video_name}")
     basket_frame_ratio = 3
@@ -332,7 +406,10 @@ if __name__ == '__main__':
     pickle.dump((jumps, player_travels, player_positions, shifted_positions), open(out_jump_file, "wb"))
     print(key_frames, file=open(out_keyframes_file, 'w'))
 
-    if not basket_location_unknown:
-        print_training_file(tmove, "move_labels.txt")
-        print_training_file(tjump, "jump_labels.txt")
-        print_training_file(tmj, "mj_labels.txt")
+    #if not basket_location_unknown:
+    print_training_file(tmove, "move_labels.txt")
+    print_training_file(tjump, "jump_labels.txt")
+    print_training_file(tmj, "mj_labels.txt")
+
+    for key in truth.keys():
+        print(key)
